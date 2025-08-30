c74ecf5d4398acf8f438690fa5424792e93ef01b
Kylie Bemis <k.bemis@northeastern.edu>
2025-08-30 17:04:05 -0400

    add update and uninstall functions

--- Library/activate.zsh
+++ Library/activate.zsh
@@ -1,4 +1,10 @@
 # Magi system environment
+# 
+# $MAGI_PREFIX sets the Magi system location
+# $MAGI_DBPATH sets the Magi data location
+# $MAGI_DBNAME sets the default data collection
+# $MAGI_USER sets the Magi cluster username
+# $MAGI_LOGIN sets the Khoury login server username
 
 magi() {
 	if [[ ! -n $MAGI_PYTHON ]]
@@ -14,4 +20,12 @@ magidb() {
 		MAGI_PYTHON="$MAGI_PREFIX/MagiSys/venv/bin/python3"
 	fi
 	eval "$MAGI_PYTHON" "$MAGI_PREFIX/MagiSys/Library/magidb.py" "$@"
+}
+
+magisys_update() {
+	eval "$MAGI_PREFIX/MagiSys/install/update.zsh"
+}
+
+magisys_uninstall() {
+	eval "$MAGI_PREFIX/MagiSys/install/uninstall.zsh"
 }
