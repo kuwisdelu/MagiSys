4cf8a436aa092f326e5ccbee1024e8478e630b2e
Kylie A. Bemis <k.bemis@northeastern.edu>
2025-09-10 17:09:12 -0400

    add askYesNo to init.zsh

--- scripts/init.zsh
+++ scripts/init.zsh
@@ -1,5 +1,34 @@
 #!/bin/zsh
 
+askYesNo() {
+	while true; do
+		if [[ -n $1 ]]
+		then
+			read "REPLY?$1 (y/n): "
+		else
+			read "REPLY?Do you want to continue? (y/n): "
+		fi
+		if [[ $REPLY = "y" ]] || [[ $REPLY = "Y" ]]
+		then
+			echo y
+			return 0
+		elif [[ $REPLY = "n" ]] || [[ $REPLY = "N" ]]
+		then
+			echo n
+			return 1
+		else
+			echo "Please enter 'y' or 'n'."
+		fi
+	done
+}
+
+echo "This command should only be run on a Magi node"
+
+if [[ $(askYesNo) == "n" ]]
+then
+	exit
+fi
+
 echo "Initializing home directory for $USER"
 
 echo "Updating user's '$HOME/.condarc'"
